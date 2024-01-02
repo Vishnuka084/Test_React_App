14c15e98a991bffbb4748a2a306bf95bfec8487c
Vishnuka yahan <vishnukayahan84@gmail.com>
2024-01-02 23:04:09 +0530

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
             <h1>Customer Data</h1>
             <hr/>
            <div className="card" >
-                <img src="..." className="card-img-top" alt="..."/>
+                <img src="https://th.bing.com/th/id/OIP.EisqckSIeTudCFYUEXCLRQHaGd?rs=1&pid=ImgDetMain" className="card-img-top" alt="..."/>
                 <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
